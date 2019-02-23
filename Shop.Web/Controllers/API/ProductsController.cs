41794dd3eafc427dfcc8b777c7bfce9d03776ab8
Pablo <Pablo@DESKTOP-E76I2ST>
2019-02-23 08:27:11 -0500

    API Products Controller

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Shop.Web.Data;
+
+    [Route("api/[Controller]")]
+
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productsRepository;
+
+        public ProductsController(IProductRepository productsRepository)
+        {
+            this.productsRepository = productsRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productsRepository.GetAll());
+        }
+    }
+}
